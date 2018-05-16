c251edcafbcb0dfb7d05a37efc259d0bf52e2681
William <willoleary6@gmail.com>
2018-05-16 18:13:29 +0100

    Little bit of work to do with using event handlers, states,props and binding

--- src/App.js
+++ src/App.js
@@ -3,16 +3,57 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+           title: 'React - Training'
+        };
+        this.onSubmit = this.onSubmit.bind(this);
+        this.changeState = this.changeState.bind(this);
+    }
+    onClick(event){
+       alert(event.target.innerHTML);
+    }
+    onSubmit(event){
+        event.preventDefault();
+        console.log(this.input.value);
+        //document.getElementById('results').innerHTML = this.input.value /*+'Submitted yo...'*/;
+    }
+    onChange(event){
+        document.getElementById('results').innerHTML = event.target.value;
+    }
+    changeState(event){
+        this.setState({
+           title: 'Rasta Spoon'
+        });
+    }
+    render() {
+        const list = ['And I wouldn’t be fonda drinking, but when I go at it I do go at it awful.. very hard. ' ,
+        'I do have 45 pints in ehh about two hours, I’d have a packet a crips then, and then maybe an ' ,
+        'ould packet of ehhh ehhhhh peanuts, and I’d go for probably…. and I’d have ten more anyway, ' ,
+        'and then i’d get up the following morning and maureen would have the fry on, and i’d go at eh ' ,
+        'again, and there’d be no fucking sthopping me, I’d take the shirt of any mans back, bastards.',
+        ];
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello world</h1>
+          <h1 className="App-title">{this.state.title}</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {
+            list.map(item => {
+                return (
+                <div className="App-intro" key={item} onClick={this.onClick}>{item}</div>
+            );
+            })
+        }
+        <form onSubmit={this.onSubmit}>
+            <input onChange={this.onChange} ref={input =>this.input = input}/>
+        </form>
+          <h1 id = 'results'></h1>
+          <div onClick={this.changeState}>Click here!</div>
       </div>
     );
   }
