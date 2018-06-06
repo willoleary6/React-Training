0ea9f6133fcc69d922dac8f588533306d94a0c3e
William <willoleary6@gmail.com>
2018-06-06 22:58:26 +0100

    Table now printing, next step is to set up reducer to append to store states than we can render multiple rows !!! :P

--- app/components/Table/index.js
+++ app/components/Table/index.js
@@ -11,63 +11,32 @@ function Table(props){
   var date = new Date();
   var timestamp = date.getTime();
   var TableData =undefined;
-  //if(props.tableData) {
-   /* let arr = [
-      {quote: "“You want weapons? We’re in a library! Books! The best weapons in the world!”", author: "The Doctor", movie: "Doctor Who"},
-  {quote: "'Engineers like to solve problems. If there are no… available, they will create their own problems.'", author: "", movie: "Scott Adams"},
-  {quote: "", author: "'You have to let it all go", movie: "The Matrix"}];*/
+
    if(props.tableData) {
-     let arr = [];
-     Object.keys(props.tableData).forEach(function (index) {
-       arr.push(props.tableData);
-     });
-     TableData = arr.map(function (index, j) {
-       return (
-         <Tr key={'row' + j + new Date().getTime()}>
-           {
-             Object.values(index).map(function (Innerindex, i) {
-               return <Td key={'column' + i + new Date().getTime()}>{Innerindex}</Td>;
-             })
-           }
-           <Td>
-             <CheckBox key={'Row ' + j + ' Deleter'}/>
-           </Td>
-         </Tr>
-       )
-     })
-     console.log(TableData);
+
+
+
+
+     //this level contains the collection of rows
+
+         //go through each row
+     TableData = Object.values(props.tableData).map(function(innerIndex,j){
+          return (
+          <Tr key={'row' + j + new Date().getTime()}>
+            {
+              //go through each Column
+              Object.values(innerIndex).map(function (innerInnerIndex, k) {
+                return <Td key={'column' + k + new Date().getTime()}>{innerInnerIndex}</Td>;
+              })
+            }
+            {
+              <Td><CheckBox id={'row'+innerIndex+'CheckBox'+new Date().getTime()}/></Td>
+            }
+          </Tr>
+          )
+        })
    }
-    /*
-    arr.map(function (index,j){
-      return(
-        <Tr key={'row' + j + new Date().getTime()}>
-          {
-            Object.values(index).map(function (Innerindex, i) {
-              return <Td key={'column' + i + new Date().getTime()}>{Innerindex}</Td>;
-            })
-          }
-        </Tr>
-      )
-    })*/
-    /*TableData = arr.map(function (index, j) {
-      return (
-        <Tr key={'row' + j}>
-          {
-            console.log(index)
-            /*Object.values(index).map(function (Innerindex, i) {
-              return <Td key={'column' + i + Innerindex}>{Innerindex}</Td>;
-            })*/
-    /*      }
-          {
-            <Td>
-              <CheckBox key={'Row ' + j + ' Deleter'}/>
-            </Td>
-          }
-        </Tr>
-      )
-    })*/
-  //}
-  //console.log(TableData);
+
   return(
     <StyledTable>
         <Thead>
@@ -78,8 +47,8 @@ function Table(props){
         </Thead>
         <Tbody>
         {
-
-      }
+          TableData
+        }
       </Tbody>
     </StyledTable>
   );
@@ -92,6 +61,24 @@ Table.propTypes = {
 export default Table;
 
 /*
+
+TableData = arr.map(function (index, i) {
+       return (
+         //go through each row
+         Object.values(index).map(function(innerIndex,j){
+          return (
+          <Tr key={'row' + j + new Date().getTime()}>
+            {
+              //go through each Column
+              Object.values(innerIndex).map(function (innerInnerIndex, k) {
+                return <Td key={'column' + k + new Date().getTime()}>{innerInnerIndex}</Td>;
+              })
+            }
+          </Tr>
+          )
+        })
+       )
+     })
 <StyledTable>
       <caption>{props.title}</caption>
       {
